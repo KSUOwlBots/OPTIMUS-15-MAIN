6180214702c6741543c5e7649528f702bae97a6f
Nikhil Gajjar <nikgajjar51@gmail.com>
2024-03-06 16:30:17 -0500

    changed to helper functions not autonomous

---
+++ include/OWL2/helper_functions.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "OWL2/robot_config.hpp"
+void wingLeftAuton(int time );
+void wingRightAuton(int time);
+void intakeControlAuton(int speed, int time);
--- include/main.h
+++ include/main.h
@@ -47,7 +47,7 @@
 #include "autons.hpp"
 #include "OWL2/robot_config.hpp"
 #include "OWL2/opcontrol.hpp"
-#include "OWL2/autonomous.hpp"
+#include "OWL2/helper_functions.hpp"
 /**
  * If you find doing pros::Motor() to be tedious and you'd prefer just to do
  * Motor, you can use the namespace with the following commented out line.
